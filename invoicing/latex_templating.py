f9db71f48478947caa64a96dd8baff8f8013173d
sarcoma <sarcoma@live.co.uk>
2018-04-28 01:09:05 +0100

    Add latex special char escape + tweaked LaTeX file

--- invoicing/latex_templating.py
+++ invoicing/latex_templating.py
@@ -10,7 +10,6 @@ Max char width for:
     client ref col = 18
 """
 
-
 class LatexTemplating:
     def __init__(self):
         self.latex_jinja_env = jinja2.Environment(
@@ -30,28 +29,50 @@ class LatexTemplating:
     def generate_quote(self, reference_code, company_name, company_address, date, total_cost, jobs):
         template = self.latex_jinja_env.get_template('templates/Quote.tex')
         print(template.render(
-            reference_code=reference_code,
-            company_name=company_name,
-            company_address=company_address,
-            date=date,
-            total_cost=re.escape(total_cost),
-            jobs=jobs
+            reference_code=self.tex_escape(reference_code),
+            company_name=self.tex_escape(company_name),
+            company_address=self.tex_escape(company_address),
+            date=self.tex_escape(date),
+            total_cost=self.tex_escape(total_cost),
+            jobs=[(lambda job: {k: self.tex_escape(v) for k, v in job.items()})(job) for job in jobs]
         ))
 
+    def tex_escape(self, text):
+        """
+            :param text: a plain text message
+            :return: the message escaped to appear correctly in LaTeX
+        """
+        conv = {
+            '&': r'\&',
+            '%': r'\%',
+            '$': r'\$',
+            '#': r'\#',
+            '_': r'\_',
+            '{': r'\{',
+            '}': r'\}',
+            '~': r'\textasciitilde{}',
+            '^': r'\^{}',
+            '\\': r'\textbackslash{}',
+            '<': r'\textless ',
+            '>': r'\textgreater ',
+        }
+        regex = re.compile(
+            '|'.join(re.escape(key) for key in sorted(conv.keys(), key=lambda item: - len(item))))
+        return regex.sub(lambda match: conv[match.group()], text)
 
 if __name__ == '__main__':
     LatexTemplating().generate_quote(
         'Q-7001',
         'Widget Corp',
-        '100 Some street,\\\\\nA city,\\\\\nA town,\\\\\nBO41 0PN',
+        '100 Some street, A city, A town, BO41 0PN',
         '14/10/2018',
         '$160',
         [{
-            'title': re.escape('Job 1'),
-            'description': re.escape('Do something'),
-            'type': re.escape('time'),
-            'estimated_time': re.escape('4h'),
-            'staff_rate': re.escape('$40'),
-            'cost': re.escape('$160')
+            'title': 'Job 1',
+            'description': 'Do something',
+            'type': 'time',
+            'estimated_time': '4h',
+            'staff_rate': '$40',
+            'cost': '$160'
         }]
     )
