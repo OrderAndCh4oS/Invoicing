041e1488de26259c476bba48877d785a429f8f61
sarcoma <sarcoma@live.co.uk>
2018-05-17 19:45:28 +0100

    Display description on quote and invoice latex files

--- invoicing/crud/quote_crud.py
+++ invoicing/crud/quote_crud.py
@@ -18,7 +18,7 @@ class QuoteCrud(BaseCrud, QuoteRepository):
         super(QuoteRepository, self).__init__('quotes')
 
     def menu(self):
-        Style.create_title('Manage ' + self.table_name)
+        title = Style.create_title('Manage ' + self.table_name)
         actions = [
             Action('1', 'View', self.show),
             Action('2', 'Add', self.add),
@@ -27,7 +27,7 @@ class QuoteCrud(BaseCrud, QuoteRepository):
             Action('5', 'Generate', self.generate),
             Action('b', 'Back', False)
         ]
-        Menu.create(actions)
+        Menu.create(title, actions)
 
     def show(self):
         print(Style.create_title('Show Quote'))
