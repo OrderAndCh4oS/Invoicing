2f99c4ef079f1433ae2ad313839685115dc59acc
sarcoma <sarcoma@live.co.uk>
2018-08-02 16:04:05 +0100

    Added Model class to validation system

--- invoicing/validator/ValidationLink.py
+++ invoicing/validator/ValidationLink.py
@@ -2,6 +2,7 @@ class Validator():
     def __init__(self):
         self.valid = False
         self.validation_links = []
+        self.error_message = None
 
     def is_valid(self):
         return self.valid
@@ -13,12 +14,30 @@ class Validator():
         for validation_link in validation_links:
             ValidationLink(self.validation_links, validation_link)
 
+    def set_error_message(self, message):
+        self.error_message = message
+
+    def get_error_message(self):
+        return self.error_message
+
     def validate(self):
+        self.error_message = None
         return self.validation_links[0](self)
 
 
 class Validation:
-    def __call__(self, data) -> Validator: pass
+    def __init__(self, error_message='is not valid'):
+        self.error_message = error_message
+
+    def __call__(self, data) -> Validator:
+        pass
+
+    def validation_check(self, data, check):
+        if not check:
+            data.set_valid(False)
+            data.set_error_message(self.error_message)
+        else:
+            data.set_valid(True)
 
 
 class ValidationLink():
@@ -42,45 +61,112 @@ class ValidationLink():
 
 
 class Data(Validator):
-    def __init__(self, data):
+    def __init__(self, validation_links, default_value=None):
         super().__init__()
-        self.data = data
+        self.value = default_value
+        self.set_validation_links(validation_links)
+
+    def set_value(self, value):
+        self.value = value
 
     def __str__(self):
-        return "Data %s Valid: %s" % (self.data, 'valid' if self.is_valid() else 'not valid')
+        return "Value: %s: %s (%s)" % (self.value, self.error_message, 'valid' if self.is_valid() else 'invalid')
 
 
 class IsRequired(Validation):
+    def __init__(self, error_message='is required'):
+        super().__init__(error_message)
+
     def __call__(self, data: Data):
-        data.set_valid(True if data.data else False)
+        self.validation_check(data, True if data.value else False)
         return data
 
 
 class IsString(Validation):
+    def __init__(self, error_message='is not a string'):
+        super().__init__(error_message)
+
     def __call__(self, data: Data):
-        data.set_valid(isinstance(data.data, str))
+        self.validation_check(data, isinstance(data.value, str))
+        return data
+
+
+class IsInteger(Validation):
+    def __init__(self, error_message='is not an integer'):
+        super().__init__(error_message)
+
+    def __call__(self, data: Data):
+        self.validation_check(data, isinstance(data.value, int))
         return data
 
 
 class MaxLength(Validation):
-    def __init__(self, max):
+    def __init__(self, max, error_message='is too long'):
+        super().__init__(error_message)
         self.max = max
 
     def __call__(self, data: Data):
-        data.set_valid(len(data.data) <= self.max)
+        self.validation_check(data, len(data.value) <= self.max)
         return data
 
 
 class MinLength(Validation):
-    def __init__(self, min):
+    def __init__(self, min, error_message='is too short'):
+        super().__init__(error_message)
         self.min = min
 
     def __call__(self, data: Data):
-        data.set_valid(len(data.data) >= self.min)
+        self.validation_check(data, len(data.value) >= self.min)
         return data
 
 
+class BaseModel:
+
+    def fields(self):
+        field_list = []
+        for k in dir(self):
+            v = getattr(self, k)
+            if isinstance(v, Data):
+                field_list.append((k, v))
+
+        return field_list
+
+    def validate(self):
+        self.errors = {}
+        for field, value in self.fields():
+            value.validate()
+            if not value.is_valid():
+                self.errors[field] = value.get_error_message()
+        self.is_valid = False if len(self.errors) else True
+
+    def __str__(self):
+        return "Errors: %s" % ', '.join(self.errors) if len(self.errors) else 'Valid'
+
+
+class Person(BaseModel):
+    first_name = Data([IsRequired(), IsString(), MaxLength(10), MinLength(3)])
+    age = Data([IsRequired(), IsInteger()])
+
+    def __init__(self, first_name, age):
+        super().__init__()
+        self.first_name.set_value(first_name)
+        self.age.set_value(age)
+
+
 if __name__ == '__main__':
-    data = Data('')
-    data.set_validation_links([IsRequired(), IsString(), MaxLength(10), MinLength(3)])
-    print(data.validate())
+    valid = Data([IsRequired(), IsString(), MaxLength(10), MinLength(3)], default_value='Hey there')
+    required = Data([IsRequired(), IsString(), MaxLength(10), MinLength(3)], default_value='')
+    not_string = Data([IsRequired(), IsString(), MaxLength(10), MinLength(3)], default_value=123)
+    too_long = Data([IsRequired(), IsString(), MaxLength(10), MinLength(3)], default_value='Hey there shit head')
+    too_short = Data([IsRequired(), IsString(), MaxLength(10), MinLength(3)], default_value='Hi')
+    print('Valid: %s' % valid.validate())
+    print('Required: %s' % required.validate())
+    print('Not a String: %s' % not_string.validate())
+    print('Too Long: %s' % too_long.validate())
+    print('Too Short: %s' % too_short.validate())
+    person = Person('John', 54)
+    person.validate()
+    print('Valid person:', person)
+    invalid_person = Person('', 'not a number')
+    invalid_person.validate()
+    print('Invalid person:', invalid_person)
