63ae1da0f440f29c1a691a45d2ef7598a60d7eb6
David Negreira <David.Negreira@emesa.nl>
2018-10-03 18:33:24 +0200

    Initial dotenv setup

--- invoicing/constants/constants.py
+++ invoicing/constants/constants.py
@@ -1,5 +1,4 @@
 from pkg_resources import resource_filename
 
 DB_PATH = resource_filename('invoicing', '/sqlite')
-DB = DB_PATH + '/invoicing_test.db'
 TEMPLATE_PATH = resource_filename('invoicing', '/templates')
